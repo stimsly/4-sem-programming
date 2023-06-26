ba1397a858abf130f56a7564b5e4f19717762790
stimsly <87019401+stimsly@users.noreply.github.com>
2023-06-26 18:56:56 +0300

    add comments and clean the code

--- algorithm/context_mixing/main.cpp
+++ algorithm/context_mixing/main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <fstream>
-#include<vector>
+#include <vector>
 #include <iomanip>
 
 using namespace std;
 
 typedef long double ld;
-string ress = "";
 
-
-/// 2 - ppm last 64 bit
-/// 3 - history ppm
-/// history - cyclic
-/// 64 bit - cyclic
+/// Most simple model. Its just said that next bit can be 0 or 1 with equal probability.
+/// By itself, this model would result in no compression.
 class model1{
 public:
     ld get(){
         return 0.5;
     }
 };
+
+/// It is make prediction based on history.
+/// Example:
+///    01011000
+/// n0 = 5  n1 = 3 -> p(0) = 5/8
 class model2{
 public:
     vector <ld> probaprobability;
@@ -39,6 +40,31 @@ public:
     }
 
 };
+
+/*
+NonstatioaryPPM. In an order n model, the next bit is
+predicted in the context of the last n - 1 bytes, plus the last
+0 to 7 bits of the current byte.  Each time a 0 or 1 occurs in some
+context, the counts n0 and n1 associated with that context is incremented.
+If the opposite count is more than 2, then the excess is halved.  This
+is a compromise between a stationary model, in which all outcomes in
+the same context are equally significant, and an inverse temporal
+model, in which the probability of an event is inversely proportional
+to the time since it last occurred, regardless of anything that happened
+previous to that.
+
+For example, suppose that a context occurs 10 times, and the sequence
+of bits that follow the context is 1111111000.  If the outcomes are
+independent, then n0 = 7, n1 = 3, and p(1) = 7/10 with confidence 10.
+In an inverse temporal model, the last 1 was 4 events ago and the last
+0 was 1 event ago, so 0 is 4 times more likely than 1, for p(1) = 1/5.
+PAQ1 uses a compromise between these two positions:
+
+  1111111     n0 = 0  n1 = 7  p(1) = 1   with confidence 7
+  11111110    n0 = 1  n1 = 4  p(1) = 4/5 with confidence 5
+  111111100   n0 = 2  n1 = 3  p(1) = 3/5 with confidence 5
+  1111111000  n0 = 3  n1 = 2  p(1) = 2/5 with confidence 5
+ */
 class model3{
 public:
     vector <ld> probaprobability;
@@ -62,6 +88,8 @@ public:
         probaprobability.resize(2);
     }
 };
+
+/// The same as the model 3 but not in gruop of 64 bits
 class model4{
 public:
     vector <ld> probaprobability;
@@ -82,6 +110,9 @@ public:
         probaprobability.resize(2);
     }
 };
+
+/// CyclicModel. The same as the model 2 but counts 0`s and 1`s in bytes representation.
+/// I.e in bit position in byte.
 class model5{
 public:
     vector <ld> cyclic_model[8];
@@ -103,6 +134,9 @@ public:
         for(auto & i : cyclic_model) i.resize(2);
     }
 };
+
+/// The same as model5.
+/// Calcs it in last 64 bits.
 class model6{
 public:
     vector <ld> cyclic_model_group[8];
@@ -128,6 +162,8 @@ public:
         for(auto & i : cyclic_model_group) i.resize(2);
     }
 };
+
+/// All models in 1 class.
 class models{
 public:
     int len = 0;
@@ -171,16 +207,19 @@ public:
         n0 += a6.get(len) * w[5];
         return n0;
     }
+    vector<ld> calc(){
+        ld n0 = get();
+        return {n0, 1 - n0};
+    }
 };
 
-vector<ld> calc(models p){
-    ld n0 = p.get();
-    return {n0, 1 - n0};
-}
 
-string s2 = "";
 
-class code {
+string s2 = "", ress = "";
+
+
+/// Arithmetic encoding and decoding
+class encode {
 public:
     ld arithmeticCoding(models p, string s) {
         ld left = 0;
@@ -188,7 +227,7 @@ public:
         for (int i = 0; i < s.size(); i++) {
             for(int j = 7; j >= 0; j--) {
                 int symb = ((1 << j) & (s[i])) != 0;
-                vector<ld> probability = calc(p);
+                vector<ld> probability = p.calc();
                 if (!symb) right = left + (right - left) * probability[0];
                 else left = left + (right - left) * probability[0];
                 s2 += char(symb + '0');
@@ -209,7 +248,7 @@ public:
         for(int i = 0; i < n; i++) {
             int res = 0;
             for(int j = 0; j < 8; j++) {
-                vector<ld> probability = calc(p);
+                vector<ld> probability = p.calc();
                 res *= 2;
                 if (c <= probability[0]) {
                     ress += '0';
@@ -232,20 +271,25 @@ public:
 
 
 int main() {
+
+    /// Geneeating random string
+
+    /// Alphabet
     string abc = "qwertyuioplkjhgfdsazxcvbnm1234567890 ";
     int len = abc.size();
+
     string s = "";
-    int n = 13;
+    int n = 1000;
     for(int i = 0; i < n; i++){
         s += char(abc[rand() % len]);
     }
-    cout << s << endl;
-    models p;
-    code a;
 
-    int g = 6;
     ofstream fout_text("text.txt");
     fout_text << s;
+
+    models p;
+    encode a;
+    int g = 6;
     ofstream fout("encode.txt");
     fout << n << endl;
     for(int i = 0; i < n; i += g){
